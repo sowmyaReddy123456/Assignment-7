162ac541dd359a2c0c61524e677dbb645262edb1
sowmyaReddy123456 <105050044+sowmyaReddy123456@users.noreply.github.com>
2022-06-16 12:13:35 +0530

    Add files via upload

---
+++ OrderControllerTest (1).java
@@ -0,0 +1,66 @@
+package com.example.demo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.terra.assignment.controller.OrderController;
+import com.terra.assignment.entity.Address;
+import com.terra.assignment.entity.ErrorMessages;
+import com.terra.assignment.entity.Item;
+import com.terra.assignment.entity.Order;
+import com.terra.assignment.entity.OrderLine;
+import com.terra.assignment.entity.OrderLineStatus;
+import com.terra.assignment.entity.OrderStatus;
+import com.terra.assignment.entity.Result;
+import com.terra.assignment.service.OrderService;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderControllerTest {
+	
+	@InjectMocks
+	OrderController controller;
+	
+	@Mock
+	OrderService orderService;
+	
+	@Test
+	void testCreateOrder() {
+		List<Item> itmList = new ArrayList<>();
+		List<Address> addList = new ArrayList<Address>();
+		Item item = new Item();
+		item.setItemId("1");
+		item.setItemName("iphone");
+		item.setPrice(150);
+		item.setQuantity("7");
+		itmList.add(item);
+		Address address = new Address();
+		address.setCity("Banglore");
+		address.setCountry("India");
+		address.setPinCode(23456);
+		addList.add(address);
+		OrderLine orderLine = new OrderLine();
+		orderLine.setEta(new Date());
+		orderLine.setOrderLineStatus(OrderLineStatus.OPEN);
+		orderLine.setListOfAddresses(addList);
+		orderLine.setListOfItems(itmList);
+		Order order = new Order();
+		order.setTotalAmount(150);
+		order.setOrderStatus(OrderStatus.OPEN);
+		order.setOrderDate(new Date());
+		when(orderService.saveOrder(order)).thenReturn(1);
+		int result = controller.createStore(order);
+		assertEquals(1, result);
+	}
+	
+
+}
